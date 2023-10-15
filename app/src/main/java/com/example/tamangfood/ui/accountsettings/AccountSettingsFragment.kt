e0f2542f01d60e21a5a0b7de12b310e666f1a057
phamhoang <pvh16.10.1999@gmail.com>
2023-10-15 19:09:38 +0700

    make ui account settings

---
+++ app/src/main/java/com/example/tamangfood/ui/accountsettings/AccountSettingsFragment.kt
@@ -0,0 +1,16 @@
+package com.example.tamangfood.ui.accountsettings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tamangfood.R
+import com.example.tamangfood.base.BaseFragment
+import com.example.tamangfood.databinding.FragmentAccountSettingsBinding
+
+class AccountSettingsFragment : BaseFragment<FragmentAccountSettingsBinding>(
+    FragmentAccountSettingsBinding::inflate
+) {
+
+}
